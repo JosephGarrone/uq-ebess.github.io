5cb82af507c70e5577f88c280ebb65019608ff53
JosephGarrone <josephgarrone@gmail.com>
2017-03-08 20:47:56 +1000

    Now has active pages. Added customizable bootstrap

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,21 @@
 const gulp = require('gulp');
 const nunjucks = require('gulp-nunjucks');
+const customizeBootstrap = require('./modified-gulp-customize-bootstrap/index.js');
+const sass = require('gulp-sass');
 
-gulp.task('compile', () => {
+gulp.task('compile', ['bootstrap'], () => {
     gulp.src(['src/templates/**/*.html', '!src/templates/**/_*.js'])
         .pipe(nunjucks.compile({}, {}))
         .pipe(gulp.dest('dist'));
     gulp.src('src/js/', { base: '.' })
-        .pipe(gulp.dest('dist/src/js/'));
+        .pipe(gulp.dest('dist/lib/js/'));
     gulp.src('src/css/', { base: '.' })
-        .pipe(gulp.dest('dist/src/css/'));
+        .pipe(gulp.dest('dist/lib/css/'));
 });
+
+gulp.task('bootstrap', () => {
+    gulp.src('node_modules/bootstrap/scss/bootstrap.scss')
+        .pipe(customizeBootstrap('src/css/bootstrap-custom/*.scss'))
+        .pipe(sass())
+        .pipe(gulp.dest('dist/lib/css/bootstrap/'))
+})
---
+++ modified-gulp-customize-bootstrap/customizeBootstrap.js
@@ -0,0 +1,48 @@
+var path = require('path');
+var _ = require('lodash');
+
+module.exports = {
+  create: function(config) {
+    // TODO: validate config
+
+    // config = {
+    //   srcPath: '',                  // The path of the source manifest
+    //   srcContent: '',               // Content of the source manifest
+    //   overrideFiles: [],            // Array of override includes
+    //   overridePath: ''              // Path of override files
+    // }
+
+    var existingManifest = this.parseExistingManifest(config.srcContent);
+    return this.processNewManifest(existingManifest, config);
+  },
+
+  parseExistingManifest: function(content) {
+    var pattern = /@import "([\w\.-]+)";/g;
+    var manifest = [];
+    var match;
+
+    // Match and store import statements
+    while ((match = pattern.exec(content)) !== null) {
+      manifest.push(match[1]);
+    }
+
+    // return manifest;
+    return manifest;
+  },
+
+  processNewManifest: function(content, config) {
+    var overridePrefix = path.relative(config.srcPath, config.overridePath);
+
+    // Iterate through manifest files
+    return content.map(function(filename) {
+      // Check if overrides matches manifest file
+      var prefix = _.includes(config.overrideFiles, filename) ? overridePrefix : './';
+      var value =  '@import "' + path.join(prefix, filename) + '";';
+      while (value.indexOf("\\") >= 0) {
+        value = value.replace("\\", "/");
+      }
+      return value;
+    }).join('\n');
+  }
+
+};
